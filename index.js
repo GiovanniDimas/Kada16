ae7f8239030933cc297c7daf605ce07665285a56
DimasG <giovannidimas32@gmail.com>
2026-02-24 14:51:42 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+
+const app = express();
+
+app.use((req,res,next) => {
+  console.log(`Request ${req.path}`);
+  next();
+})
+
+app.use((req,res,next)=> {
+  if(false){
+    next(new Error('Not Authorized'));
+    return;
+  }
+  next();
+}) 
+
+app.use((err,req,res,next) => {
+  res.send('Error Occured');
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+app.get('/halo/:greeting', (req, res) => {
+  const { greeting } = req.params;
+  res.send(`Hello ${greeting}!`);
+});
+
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
